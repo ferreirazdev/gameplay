ebc92e61d9d31793a2f37fecccdb7b91fa32e003
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-25 15:03:07 -0300

    New Components

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -10,7 +10,8 @@ import { theme } from "../../global/styles/theme";
 
 type Props = RectButtonProps & {
   title: string;
-  icon: React.FC<SvgProps>
+  icon: React.FC<SvgProps>;
+  hasCheckBox?: boolean;
   checked?: boolean;
 }
 
@@ -19,9 +20,10 @@ export function Category({
   title,
   icon: Icon,
   checked = false,
+  hasCheckBox = false,
   ...rest
 }: Props) {
-  const { secondary50, secondary70 } = theme.colors
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors
   return (
     
 
@@ -30,15 +32,21 @@ export function Category({
         style={styles.container}
         colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4}]}>
-          <View style={checked ? styles.checked : styles.check} />
+        <LinearGradient
+          style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[ checked ? secondary85 : secondary50, secondary40 ]}
+        > 
+          { 
+            hasCheckBox &&
+            <View style={checked ? styles.checked : styles.check} />
+          }
           <Icon width={48} height={48} />
           <Text style={styles.title}>
             { title }
            </Text>
-        </View>
+        </LinearGradient>
         
       </LinearGradient>
-    </RectButton>
+    </RectButton> 
   );
 }
