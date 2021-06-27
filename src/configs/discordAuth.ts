e04d57ff8c8cd3ef812681c6a5be923fcac499d1
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-26 23:07:07 -0300

    Create discordAuth.ts

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https://auth.expo.io/@anonymous/gameplay-90dec53d-2f62-448a-a1ce-e3643d8fa1f2';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '858412102846119966';
+const CDN_IMAGE = 'https://cdn.dicordapp.com';
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
