9a8e55e2fc442362218a78ec20077dd51cfc4e79
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-06-27 11:00:18 -0300

    guild api connection

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,17 +1,35 @@
-import React from "react";
-import { RectButton, RectButtonProps } from "react-native-gesture-handler";
-import { Image } from "react-native";
+import React from 'react';
+import { Image, View } from 'react-native';
 
-import { styles } from "./styles";
+import { styles } from './styles';
+import DiscordSvg from '../../assets/discord.svg';
 
+const { CDN_IMAGE } = process.env;
 
-export function GuildIcon() {
-  const uri = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmforum.ist%2Fmedia%2Fdiscord-logo-png.9007%2Ffull&f=1&nofb=1';
+type Props = {
+  guildId: string;
+  iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
+
   return (
-    <Image
-      source={{ uri }}
-      style={styles.image}
-      resizeMode="cover"
-    />
+    <View style={styles.container}>
+      {  
+        iconId ?    
+        <Image 
+          source={{ uri }}
+          style={styles.image}
+          resizeMode="cover"    
+        />
+        :
+        <DiscordSvg 
+          width={40} 
+          height={40}
+        />
+      }
+    </View>
   )
+
 }
